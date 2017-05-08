4d1ccc32aa324be71a9dadd6a38111ba2a6d503e
Piffek <patrykpiwko123412@gmail.com>
2017-05-08 14:53:46 +0200

    create form to not loggin user realization

--- src/AppBundle/Form/RealizationBuyingFormType.php
+++ src/AppBundle/Form/RealizationBuyingFormType.php
@@ -1,4 +1,4 @@
-<?php 
+﻿<?php 
 namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -9,25 +9,15 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
-class RegisterForm extends AbstractType
+class RealizationBuyingFormType extends AbstractType
 {
 	public function buildForm(FormBuilderInterface $builder, array $options)
 	{
 		$builder
-		->add('username', TextType::class)
-		->add('email', EmailType::class)
 		->add('name', TextType::class)
+		->add('email', EmailType::class)
 		->add('city', TextType::class)
-		->add('street', TextType::class)
-		->add('plainPassword', RepeatedType::class,[
-				'type'=>PasswordType::class
-		]);
-	}
-	public function configureOptions(OptionsResolver $resolver)
-	{
-		$resolver->setDefaults([
-				'data_class' => User::class
-		]);
+		->add('street', TextType::class);
 	}
 }
 
