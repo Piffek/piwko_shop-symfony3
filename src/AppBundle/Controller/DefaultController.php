f79c481907d5ac53b42be39d166b736edc89036b
Piffek <patrykpiwko123412@gmail.com>
2017-06-29 22:52:24 +0200

    ref

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,7 +6,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use AppBundle\Form\SearchUserForm;
 
 class DefaultController extends Controller
 {
@@ -26,35 +25,5 @@ class DefaultController extends Controller
 			'products' => $products,
 		]);
     }
-    
 
-    
-	 /**
-     * @Route("/profile", name="userprofile")
-     */
-    public function searchAction(Request $request)
-    {
-		$form = $this->createForm(SearchUserForm::class);
-		$form->handleRequest($request);
-		if($form->isValid() && $form->isSubmitted()){
-			
-			$username = $form->getData();
-			
-			if(isset($username)){
-				$searchUser = $this->getDoctrine()->getManager()->createQueryBuilder()
-					->select('p')
-					->from('AppBundle:User', 'p')
-					->where('p.username LIKE :username')
-					->setParameter('username', '%'.$username['_username'].'%')
-					->getQuery()
-					->getResult();	
-			}
-			
-		}
-    	
-		return $this->render('profile/userProfile.html.twig', [
-			'form' => $form->createView(),
-			'searchUser' => isset($searchUser) ? $searchUser : ' ',
-		]);
-    }
 }
