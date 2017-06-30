076a6b979d81925b4306a1c0b34e03315e206448
Piffek <patrykpiwko123412@gmail.com>
2017-06-30 14:04:57 +0200

    add assets

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
         	new JMS\Payment\PaypalBundle\JMSPaymentPaypalBundle(),
         	new WhiteOctober\TCPDFBundle\WhiteOctoberTCPDFBundle(),
 			new Vich\UploaderBundle\VichUploaderBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new AppBundle\AppBundle(),
         ];
 
