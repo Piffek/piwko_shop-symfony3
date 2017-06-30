7eb23ff549059f27d8fbb841fea05c5e0787f995
Piffek <patrykpiwko123412@gmail.com>
2017-06-30 14:48:05 +0200

    ref

--- src/AppBundle/Form/LoginFormType.php
+++ src/AppBundle/Form/LoginFormType.php
@@ -5,7 +5,7 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class LoginFormType extends AbstractType
 {
@@ -14,7 +14,7 @@ class LoginFormType extends AbstractType
      */
 	public function buildForm(FormBuilderInterface $builder, array $options){
 		$builder
-		->add('_username')
+		->add('_username', EmailType::class)
 		->add('_password', PasswordType::class);
 	}
 }
